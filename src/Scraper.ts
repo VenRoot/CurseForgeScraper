d035981c30328cefc62135aa82ed50856b7b5774
Ven <0ventox0@gmail.com>
2023-09-14 22:12:47 +0200

    Updated

--- src/Scraper.ts
+++ src/Scraper.ts
@@ -1,45 +1,136 @@
-class Scraper 
-{
-    private baseURL: string;
-    private path: string;
-    private parameters: Record<string, string>;
-    private modNames: string[];
-    private URLs: string[];
+import ResponseData from "./curseforge.type";
+import fs from "fs/promises";
+export default class Scraper {
+  private baseURL: string;
+  private path: string;
+  private parameters: Record<string, string>;
+  private modNames: string[];
+  public URLs: string[];
 
-    constructor(baseURL: string, path: string, parameters: Record<string, string>, modNames: string[])
-    {
-        this.baseURL = baseURL;
-        this.path = path;
-        this.parameters = parameters;
-        this.modNames = modNames;
-        this.URLs = [];
+  constructor(
+    baseURL: string,
+    path: string,
+    parameters: Record<string, string>,
+    modNames: string[]
+  ) {
+    this.baseURL = baseURL;
+    this.path = path;
+    this.parameters = parameters;
+    this.modNames = modNames;
+    this.URLs = [];
 
-        modNames.forEach((modName) => {
+    modNames.forEach((modName) => {
+      const parameter: Record<string, string> = {
+        ...this.parameters,
+        filterText: modName,
+      };
+      const url =
+        this.baseURL + this.path + new URLSearchParams(parameter).toString();
+        this.URLs.push(url);
+    });
+  }
 
-            const parameter: Record<string, string> = {
-                ...this.parameters,
-                "searchFilter": modName
-            };
-            const url = this.baseURL + this.path + new URLSearchParams(parameter).toString();
-            this.URLs.push(url);
-        });
+  private getAllGameVersions(data: ModResponse): string[] {
+    let gameVersionsSet: Set<string> = new Set();
+  
+    for (const recentFile of data.data[0].websiteRecentFiles) {
+      for (const file of recentFile.files) {
+        for (const version of file.gameVersions) {
+          // Use a regex to check if the element is a game version
+          if (/^\d+\.\d+(\.\d+)?$/.test(version)) {
+            gameVersionsSet.add(version);
+          }
+        }
+      }
     }
-    
+  
+    return Array.from(gameVersionsSet);
+  }
+  
 
-    public async scrape(): Promise<Record<string, string>>
-    {
-        this.URLs.forEach(async url => {
-            await fetch(url, {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Accept": "application/json"
-                }
-            })
-        })
+  public async scrape(): Promise<ModInfo[]> {
+    const mods: ModInfo[] = [];
+
+    await Promise.all(
+      this.URLs.map(async (url) => {
+        const response = await fetch(url);
+        const json = (await response.json()) as ModResponse;
+        if(json.data.length === 0)
+        {
+          console.log(`No mod found for ${url}`);
+          return;
+        }
+
+        let gameVersions = this.getAllGameVersions(json);
+
+        const mod: ModInfo = {
+          name: json.data[0].name,
+          id: json.data[0].id,
+          // gameVersions: json.data[0].latestFileDetails.gameVersions,
+          gameVersions: gameVersions,
+          url: "https://www.curseforge.com/minecraft/mc-mods/"+json.data[0].slug
+        };
+
+        mods.push(mod);
+      })
+    );
+    return mods;
+  }
+
+  public async save(mods: ModInfo[]) {
+    const json = JSON.stringify(mods, null, 2);
+    await fs.writeFile("./mods.json", json);
+  }
+
+  public getMostCommonGameVersion(mods: ModInfo[]): [string, number] {
+    const versionCounts: { [version: string]: number } = {};
+
+    for (const mod of mods) {
+      for (const version of mod.gameVersions) {
+        if (version in versionCounts) {
+          versionCounts[version]++;
+        } else {
+          versionCounts[version] = 1;
+        }
+      }
     }
 
+    let mostCommonVersion = "";
+    let maxCount = 0;
 
+    for (const [version, count] of Object.entries(versionCounts)) {
+      if (count > maxCount) {
+        mostCommonVersion = version;
+        maxCount = count;
+      }
+    }
 
+    return [mostCommonVersion, maxCount];
+  }
 
+  public displayIncompatibleMods(mods: ModInfo[], gameVersion: string) {
+    let localMods = mods.filter((mod) => mod.gameVersions.includes(gameVersion));
+    console.log(`Mods incompatible with ${gameVersion}: (${localMods.length})`);
 
+    for (const mod of localMods) {
+        console.log(`- ${mod.name} (ID: ${mod.id})`);
+    }
+    console.log("\n");
+  }
+}
+
+interface ModResponse {
+  data: ResponseData[];
+  pagination: {
+    index: number;
+    pageSize: number;
+    totalCount: number;
+  };
+}
+
+interface ModInfo {
+  name: string;
+  id: number;
+  gameVersions: string[];
+  url: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,138 @@
-import fs from "fs";
-import type CurseForgeResponse from "./curseforge.type";
+import Scraper from "./Scraper";
 
 const baseURL = "https://curseforge.com";
-const path = "/v1/mods/search";
-// const APIKey = "$2a$10$LlNNC67NDOE/dP9m0ydZEuaMHTGSlyfrAsXfBuaplAJMYN6nVu6vu";
-
+const path = "/api/v1/mods/search?";
 const parameters: Record<string, string> = {
     "gameId": "432", // 432 = Minecraft
-    "searchFilter": "",
     "index": "0",
-    "filterText": "20",
-    "categoryIds[0]": "421",
-    "classId": "6" //Only show mods
+    "classId": "6", //Only show mods
+    "filterText": "", // The search query
+    "pageSize": "20",
+    "sortField": "1" 
 
-}
-
-const query = new URLSearchParams(parameters).toString();
-
-fetch(baseURL + "/api/v1/mods/search?"+query, {
-    headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    },
-}).then(async (response) => {
-    if(response.ok)
-    {
-        const json = await response.json();
-        console.log(json);
-        fs.writeFileSync("./games.json", JSON.stringify(json, null, 2));
-    }
-    else
-    {
-        console.log(response.statusText);
-    }
-});
+};
 
 
 
-// fetch(baseURL + path + "?gameId=" + parameters.gameId + "&searchFilter=" + parameters.searchFilter + "&APIKey=" + APIKey)
+const modNames = [
+    "ambient sound 5",
+    "auditory",
+    "better biome blend",
+    "effective",
+    "enhanced visuals",
+    "extra sounds",
+    "fabric seasons",
+    "blur",
+    "falling leaves",
+    "illuminations",
+    "immersive weathering",
+    "lambda better grass",
+    "more mob variants",
+    "presence footsteps",
+    "sound physics remastered",
+    "vanilla tweaks",
+    "more axolotl variants",
+    "visuality",
+    "advancement plaques",
+    "anvil restoration",
+    "apple skin",
+    "areas",
+    "axolotl bucket fix",
+    "basic shields",
+    "bedspreads",
+    "better advancements",
+    "better animations collection",
+    "better statistics screen",
+    "better third person",
+    "better tridents",
+    "camera overhaul",
+    "camera utils",
+    "comforts",
+    "connectible chains",
+    "customizable elytra",
+    "damage tilt",
+    "deepslate cutting",
+    "diagonal fences",
+    "display case",
+    "easy anvils",
+    "easy magic",
+    "eating animation",
+    "emerald apple",
+    "enchantment descriptions",
+    "first-person model",
+    "gilded armor",
+    "herds panic",
+    "inmis",
+    "inmis addon",
+    "inventory profiles next",
+    "inv mode",
+    "iron bows",
+    "leaves be gone",
+    "mod name tooltip",
+    "more banner features",
+    "mouse wheelie",
+    "not enough animations",
+    "physics mod pro",
+    "random village names",
+    "repurposed structures",
+    "right click harvest",
+    "ruined equipment",
+    "shulker box tooltip",
+    "skinned lanterns",
+    "smooth swapping",
+    "soul fired",
+    "terralith",
+    "nyf's spiders",
+    "tooltip rareness",
+    "trinkets",
+    "universal bone meal",
+    "village bell recipe",
+    "villager names",
+    "void totem",
+    "ydm's weapon master",
+    "yung's extras",
+    "yung's better dungeons",
+    "yung's better mineshafts",
+    "yung's better strongholds",
+    "cherished worlds",
+    "cloth config api",
+    "cherished worlds",
+    "collective",
+    "continuity",
+    "creative core",
+    "creative fly",
+    "dashloader",
+    "fabric api",
+    "fabric language kotlin",
+    "fabric shield lib",
+    "ferrite core",
+    "forge config api port",
+    "iceberg",
+    "indium",
+    "iris",
+    "jam lib",
+    "kiwi",
+    "lazydfu",
+    "lib ipn",
+    "lithium",
+    "mod menu",
+    "moonlight lib",
+    "paxi",
+    "puzzles lib",
+    "replay mod",
+    "sodium",
+    "yung's api"
+];
 
+
+
+const scraper = new Scraper(baseURL, path, parameters, modNames);
+
+console.log(scraper.URLs);
+
+scraper.scrape().then((mods) => {
+    scraper.displayIncompatibleMods(mods, "1.20");
+    scraper.displayIncompatibleMods(mods, "1.19");
+    scraper.displayIncompatibleMods(mods, "1.18");
+    scraper.save(mods);
+})
